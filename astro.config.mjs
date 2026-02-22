5cb84c6ef2486cb03f0baca37386ac97ba621aa4
RHGDev <21209674+RHGDEV@users.noreply.github.com>
2026-02-21 22:14:56 -0500

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://rusty.huskey.cc',
 	vite: {
+		// @ts-ignore
 		plugins: [tailwindcss()],
 	},
 });
