eae034ef8d828c1a34e61bdc98a88830618cfe5a
RHGDev <21209674+RHGDEV@users.noreply.github.com>
2026-02-22 22:16:57 -0500

    config update

--- src/config.ts
+++ src/config.ts
@@ -1,47 +1,46 @@
 export const siteConfig = {
   name: "Rusty Huskey",
   title: "Computer IT Networking Graduate",
-  description: "Portfolio website of Rusty",
+	location: "Tennessee, USA",
   aboutMe:
-    "tbd",
+    "Aspiring IT professional with a passion for computer networking and a strong foundation in troubleshooting, network configuration, and security. Eager to apply my skills and knowledge in a dynamic IT environment while continuously learning and growing in the field.",
   skills: ["To do"],
-
-  projects: [
-    {
-      name: "WS Hopper Server",
-      description:
-        "tbd",
-      // link: "",
-      skills: ["tbd"],
-    }
-  ],
 
 	backgrounds: { // IDs of images on Unsplash
     default: "1631677209832-e9dd6aec3697",
     summer: [
-      "1541843713287-e0d5de49a384",
-      "1512848857248-75eff50a19a1",
-      "1544513169-0a660c85bdd9",
-    ],
-    spring: [
-      "1591194577912-2c7811d0f994",
-      "1541942172213-60aeb3558797",
-      "1597086454462-1f9b6e95e1f7",
-    ],
-    autumn: [
-      "1507486411790-179bbb6866ed",
-      "1550931937-2dfd45a40da0",
-      "1508766229-1a45d4127740",
-    ],
-    winter: [
-      "1515583484859-a15e976f80d2",
-      "1483921020237-2ff51e8e4b22",
-      "1548438444-275b68ae5120",
-    ],
+		'1541843713287-e0d5de49a384',
+		'1512848857248-75eff50a19a1',
+		'1544513169-0a660c85bdd9',
+	],
+	spring: [
+		'1591194577912-2c7811d0f994',
+		'1541942172213-60aeb3558797',
+		'1597086454462-1f9b6e95e1f7',
+		'1520363027780-797c0e65eed5',
+	],
+	autumn: [
+		'1507486411790-179bbb6866ed',
+		'1550931937-2dfd45a40da0',
+		'1508766229-1a45d4127740',
+		'1509838174235-432f709c7bfd',
+		'1634523453794-b992ac716fa3',
+		'1633622667733-5de5e7ab9305',
+		'1667748941477-28964059b86e',
+		'1733503757968-b2281fbdd10b',
+	],
+	winter: [
+		'1515583484859-a15e976f80d2',
+		'1483921020237-2ff51e8e4b22',
+		'1456389573961-0ae56d45961e',
+		'1641785680273-9017dd4cb5c9',
+		'1548438444-275b68ae5120',
+	],
   },
 
 	navBar: [
 		{ name: "About", link: "#about" },
+		{ name: "Credentials", link: "#credentials" },
 		{ name: "Projects", link: "#projects" },
 	],
 };
