f654573b69c6feca77e486a2824e8bd987c08168
RHGDev <21209674+RHGDEV@users.noreply.github.com>
2026-02-23 15:15:00 -0500

    Add icon support/simpleicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rusty.huskey.cc',
+
 	vite: {
 		// @ts-ignore
 		plugins: [tailwindcss()],
 	},
+
+	integrations: [icon()],
 });
