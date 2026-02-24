d9cc47ee9595dfcb91d5467b84fec801fd694fc9
RHGDev <21209674+RHGDEV@users.noreply.github.com>
2026-02-24 10:44:41 -0500

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rusty.huskey.cc',
+    site: 'https://rusty.huskey.cc',
 
-	vite: {
-		// @ts-ignore
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        // @ts-ignore
+        plugins: [tailwindcss()],
+    },
 
-	integrations: [icon()],
+    integrations: [icon(), sitemap()],
 });
